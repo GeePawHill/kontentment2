4659d7851cec3e5a96e4ad28c70e30f32467b290
GeePaw Hill <geepawhill@geepawhill.org>
2023-05-01 22:35:36 -0400

    Now a JavaFx app using TornadoFx for its internals.

--- app/src/main/kotlin/org/geepawhill/kontentment/app/Main.kt
+++ app/src/main/kotlin/org/geepawhill/kontentment/app/Main.kt
@@ -1,8 +1,17 @@
 package org.geepawhill.kontentment.app
 
-class Main {
+import javafx.application.Application
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+class Main : Application() {
+    override fun start(primaryStage: Stage?) {
+        val view = MainView().root
+        primaryStage!!.scene = Scene(view)
+        primaryStage.show()
+    }
 }
 
-fun main() {
-
+fun main(args:Array<String>) {
+    Application.launch(Main::class.java,*args)
 }
---
+++ app/src/main/kotlin/org/geepawhill/kontentment/app/MainView.kt
@@ -0,0 +1,11 @@
+package org.geepawhill.kontentment.app
+
+import javafx.scene.Parent
+import tornadofx.*
+
+class MainView : Fragment() {
+    override val root: Parent = borderpane {
+
+    }
+
+}
